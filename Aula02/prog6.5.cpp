362bbdf3417335ef3780e9a117aaf143dc50bfe7
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2023-05-25 20:58:17 -0300

    Add files via upload

--- Aula02/prog6.5.cpp
+++ Aula02/prog6.5.cpp
@@ -6,15 +6,17 @@ vendido e mais 5% do valor das vendas por ele efetuadas. Escrever um algoritimo
 que leia o numero de carros por ele vendidos, o valor total de suas vendas
 */
 
+//OBS: essa e a minha versao do exercicio, que fiz mais simplificado porem sem comissao 1 e 2 no printf final
+
 int ncarros;
 float valorTotal; /*vendedores ganham 5% -> * 5)/100*/
 float salFixo;
 float valorcarro;
-
+float salariofinal;
 int main(){
 
 	printf("Digite o numero de carros que voce vendeu:");
-	scanf("%d",&carros);
+	scanf("%d",&ncarros);
 	
 	printf("Digite o valor total de suas vendas:");
 	scanf("%f",&valorTotal);
@@ -25,9 +27,9 @@ int main(){
 	printf("Digite seu salario fixo:");
 	scanf("%f",&salFixo);
 	
-	valorTotal = (valorcarro * ncarros) + (valorTotal * 5)/100 + salFixo;
+	salariofinal = (valorcarro * ncarros) + (valorTotal * 5)/100 + salFixo;
 
-	printf("Salario Final: %0.2f \n:");
+	printf("Salario Final: %0.2f \n:", salariofinal);
 
 
 	return 0;
