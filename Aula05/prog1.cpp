6ae5684324b8f13fd6c578712cd7a5a96bd5d655
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2023-06-06 18:11:23 -0300

    Add files via upload

---
+++ Aula05/prog1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/*
+Escreva um programa que leia duas notas de um aluno.
+
+A) Calcular a media do aluno.
+
+B) Imprimir o situação do aluno em relação a media encontrada:
+	media >= 7 'aluno aprovado'
+	media < 4 'aluno reprovado'
+	media entre 4 e 7 'aluno em recuperação'
+
+int main(){
+	
+	
+	
+	return 0;
+}
