a988cfb6e465bf25fb11774dd14e30bd70eb0758
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2023-06-15 20:57:03 -0300

    Update prog1.cpp

--- Aula07_online/prog1.cpp
+++ Aula07_online/prog1.cpp
@@ -1,31 +1,46 @@
 #include<stdio.h>
 /*
+Escreva um programa que leia o peso
+e a altura de uma pessoa.
 
+A) Calcular o IMC da pessoa.
+B) Imprimir o IMC encontrado.
 
+IMC = peso / altura * altura
 
 */
 
-float altura;
 float peso;
+float altura;
 
 void entrada(){
-	printf("Digite seu peso");
-	scanf("%f",&peso);
-	printf("Digite sua altura");
-	scanf("%f",&altura);
+	printf("Digite o peso : ");
+	scanf("%f", &peso);
+	
+	printf("Digite a altura : ");
+	scanf("%f", &altura);
+		
 }
 
-float imc(){
-	
+float calcularIMC(){
+	float imc = 0;
+	imc = peso / (altura * altura);
+	return imc;
 }
 
 void saida(){
-	printf
+	printf("IMC : %0.2f \n", calcularIMC());
 }
 
 int main(){
-	
-	
-	
+	entrada();
+	saida();
 	return 0;
 }
+
+
+
+
+
+
+
