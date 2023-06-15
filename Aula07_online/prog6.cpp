17a5793f52076230127d3fb946c424777e29d6d1
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2023-06-15 20:57:45 -0300

    Update prog6.cpp

--- Aula07_online/prog6.cpp
+++ Aula07_online/prog6.cpp
@@ -1,6 +1,9 @@
 #include<stdio.h>
 /*
-
+Ler o salário fixo e o valor das vendas efetuadas pelo vendedor de uma empresa. 
+Sabendo-se que ele recebe uma comissão de 3% sobre o total das vendas 
+até R$ 1.500,00 mais 5% sobre o que ultrapassar este valor, calcular 
+e escrever o seu salário total.
 */
 
 float salFixo;
